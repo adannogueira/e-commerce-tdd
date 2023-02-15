317ec552c9240b911fe8d08989dd769cde545cd0
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-14 21:38:17 -0300

    refactor: organize methods

--- src/cpf.ts
+++ src/cpf.ts
@@ -6,18 +6,18 @@ export class Cpf {
     if (!this.validate()) throw new Error('Invalid CPF')
   }
 
-  getValue() {
-    return this.value.replace(REMOVE_NON_NUMBERS_REGEX, '');
-  }
-
-  validate (): boolean {
+  public validate (): boolean {
     if (!this.value) return false
-    const parsedCpf = this.value.replace(REMOVE_NON_NUMBERS_REGEX, '')
+    const parsedCpf = this.getValue()
     if (parsedCpf.length !== CPF_LENGTH) return false
     const allDigitsTheSame = [...parsedCpf].every(digit => digit === parsedCpf[0])
     if (allDigitsTheSame) return false
     const digits = this.calculateDigit(parsedCpf.substring(0, 9))
     return digits === parsedCpf.substring(9, 11)
+  }
+
+  public getValue() {
+    return this.value.replace(REMOVE_NON_NUMBERS_REGEX, '');
   }
 
   private calculateDigit (parsedCpf: string): string {
@@ -27,8 +27,7 @@ export class Cpf {
   }
 
   private calculateDigitAt (parsedCpf: string, index: number): string {
-    const sum = parsedCpf
-      .split('')
+    const sum = [...parsedCpf]
       .map((digit, i) => parseInt(digit) * (10 - i + index))
       .reduce((acc, curr) => acc + curr, 0)
     return (sum * 10) % 11 < 10 ? (sum * 10) % 11 + '' : '0'
