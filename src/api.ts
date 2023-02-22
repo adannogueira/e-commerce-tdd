983b8fef46687c342052af919198dc8afe03e12b
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-22 08:09:41 -0300

    refactor: main into api

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { Checkout } from './Checkout';
+import { MyDatabase } from './Database';
+
+const app = express();
+app.use(express.json());
+
+app.post("/checkout", async function (req, res) {
+	const database = new MyDatabase();
+	const checkout = new Checkout(database, database);
+	try {
+		const output = await checkout.execute(req.body);
+		res.json(output);
+	} catch (error: any) {
+		res.status(422).json({ message: error.message })
+	}
+});
+
+app.listen(3000);
