3121c4785a837d96bfac8e308ddda60fdbf6f375
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-22 08:09:20 -0300

    feat: add Checkout class

---
+++ src/Checkout.ts
@@ -0,0 +1,59 @@
+import { CouponData } from './CouponData';
+import { validate } from './CpfValidator';
+import { CurrencyGateway } from './CurrencyGateway';
+import { ProductData } from './ProductData';
+
+export class Checkout {
+	constructor(
+		private readonly productData: ProductData,
+		private readonly couponData: CouponData
+	) {}
+
+	public async execute (input: Input) {
+		const isValid = validate(input.cpf);
+		if (!isValid) {
+			throw new Error("Invalid cpf");
+		}
+		let total = 0;
+		let freight = 0;
+		const currencyGateway = new CurrencyGateway();
+		const currencies: any = await currencyGateway.getCurrencies();
+		for (const item of input.items) {
+			if (input.items.filter(({ idProduct }: { idProduct: number }) => idProduct === item.idProduct).length > 1) {
+				throw new Error("Invalid cart");
+			}
+			if (item.quantity <= 0) {
+				throw new Error("Invalid product quantity");
+			}
+			const product = await this.productData.getProduct(item.idProduct);
+			if (product) {
+				if (product.largura < 0 || product.altura < 0 || product.profundidade < 0) {
+					throw new Error("Invalid product dimension");
+				}
+				if (product.peso < 0) {
+					throw new Error("Invalid product weight");
+				}
+				total += (parseFloat(product.price) * item.quantity) * currencies[product.currency];
+				const volume = (parseInt(product.largura) * parseInt(product.altura) * parseInt(product.profundidade)) * 0.000001;
+				const densidade = parseInt(product.peso) / volume;
+				const frete = 1000 * volume * (densidade / 100);
+				freight += frete > 10 ? frete : 10;
+			} else {
+				throw new Error("Product not found");
+			}
+		}
+		if (input.coupon) {
+			const coupon = await this.couponData.getCoupon(input.coupon);
+			if (coupon && new Date(coupon.expiresIn) >= new Date()) {
+				total -= (total * coupon.percentage)/100;
+			}
+		}
+		return { total,	freight: Math.round(freight) };
+	}
+}
+
+type Input = {
+  cpf: string,
+  items: { idProduct: number, quantity: number }[],
+  coupon?: string
+}
---
+++ test/checkout.test.ts
@@ -0,0 +1,176 @@
+import { Checkout } from '../src/Checkout';
+import { CouponData } from '../src/CouponData';
+import { CurrencyGateway } from '../src/CurrencyGateway';
+import { ProductData } from '../src/ProductData';
+
+class Product implements ProductData, CouponData {
+	getProduct(idProduct: number): Promise<any> {
+		const products: any = {
+			1: { id_product: 1, description: 'Camera', price: 1000, largura: 20, altura: 15, profundidade: 10, peso: 1, currency: 'BRL' },
+			2: { id_product: 2, description: 'Geladeira', price: 5000, largura: 200, altura: 10, profundidade: 50, peso: 40, currency: 'BRL' },
+			3: { id_product: 3, description: 'Guitarra', price: 30, largura: 100, altura: 30, profundidade: 10, peso: 3, currency: 'BRL' },
+			4: null,
+			5: { id_product: 5, description: 'D', price: 30, largura: -100, altura: 30, profundidade: 10, peso: 3, currency: 'BRL' },
+			6: { id_product: 6, description: 'E', price: 30, largura: 100, altura: 30, profundidade: 10, peso: -3, currency: 'BRL' },
+			7: { id_product: 7, description: 'Guitar', price: 30, largura: 100, altura: 30, profundidade: 10, peso: 3, currency: 'USD' },
+		}
+		return products[idProduct];
+	}
+
+	getCoupon(coupon: string): Promise<any> {
+		const coupons: any = {
+			VALE20: { code: 'VALE20', percentage: 20, expiresIn: '2024-01-01' },
+			VALE10: { code: 'VALE10', percentage: 10, expiresIn: '2022-01-01' }
+		}
+		return coupons[coupon];
+	}
+}
+const checkout = new Checkout(new Product(), new Product());
+
+describe('Checkout()', () => {
+	test("Não deve fazer um pedido com cpf inválido", async function () {
+		const input: any = {
+			cpf: "987.654.321-01"
+		};
+		const promise = checkout.execute(input);
+		await expect(promise).rejects.toThrow("Invalid cpf");
+	});
+	
+	test("Deve fazer um pedido com 3 produtos", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 }
+			]
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(6090);
+	});
+	
+	test("Não deve fazer pedido com produto que não existe", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 4, quantity: 1 }
+			]
+		};
+		const promise = checkout.execute(input);
+		await expect(promise).rejects.toThrow("Product not found");
+	});
+	
+	test("Deve fazer um pedido com 3 produtos com cupom de desconto", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 }
+			],
+			coupon: "VALE20"
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(4872);
+	});
+	
+	test("Não deve aplicar um cupom de desconto expirado", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 }
+			],
+			coupon: "VALE10"
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(6090);
+	});
+	
+	test("Não deve fazer um pedido com uma quantidade negativa de produtos", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: -1 }
+			]
+		};
+		const promise = checkout.execute(input);
+		await expect(promise).rejects.toThrow("Invalid product quantity");
+	});
+	
+	test("Não deve fazer um pedido quando um produto é informado mais de uma vez", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 1, quantity: 2 }
+			]
+		};
+		const promise = checkout.execute(input);
+		await expect(promise).rejects.toThrow("Invalid cart");
+	});
+	
+	test("Não deve fazer um pedido quando alguma dimensão do produto é negativa", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 5, quantity: 1 }
+			]
+		};
+		const promise = checkout.execute(input);
+		await expect(promise).rejects.toThrow("Invalid product dimension");
+	});
+	
+	test("Não deve fazer um pedido quando o peso do produto é negativo", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 6, quantity: 1 }
+			]
+		};
+		const promise =  checkout.execute(input);
+		await expect(promise).rejects.toThrow("Invalid product weight");
+	});
+	
+	test("Não deve ter valor de frete menor que $10", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 }
+			]
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(1000);
+		expect(output.freight).toBe(10);
+	});
+	
+	test("Deve calcular valor de frete com base nos produtos", async function () {
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 2, quantity: 1 }
+			]
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(5000);
+		expect(output.freight).toBe(400);
+	});
+
+	test("Deve fazer um pedido com 4 produtos e com moedas diferentes", async function () {
+		jest
+			.spyOn(CurrencyGateway.prototype, 'getCurrencies')
+			.mockResolvedValueOnce({ USD: 3, BRL: 1 });
+		const input = {
+			cpf: "987.654.321-00",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+				{ idProduct: 7, quantity: 1 }
+			]
+		};
+		const output = await checkout.execute(input);
+		expect(output.total).toBe(6180);
+	});
+});
