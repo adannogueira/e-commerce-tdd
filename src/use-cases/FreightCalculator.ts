b4b946294d40a4a7d7d25c0d31f1fac0a5c436c7
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-27 21:29:52 -0300

    feat: calculate freight from input

--- src/use-cases/FreightCalculator.ts
+++ src/use-cases/FreightCalculator.ts
@@ -1,16 +1,24 @@
-import { Product } from '../ProductData';
+import { Product, ProductData } from '../ProductData';
 
 export class FreightCalculator {
-  execute(product: Product) {
-    if (product.largura < 0 || product.altura < 0 || product.profundidade < 0) {
-      throw new Error("Invalid product dimension");
+  constructor(private readonly productData: ProductData) {}
+
+  async execute(items: { idProduct: number, quantity: number }[]) {
+    let freight = 0;
+    for (const item of items) {
+      const product = await this.productData.getProduct(item.idProduct);
+      if (!product) throw new Error('Product not found')
+      if (product.largura < 0 || product.altura < 0 || product.profundidade < 0) {
+        throw new Error('Invalid product dimension');
+      }
+      if (product.peso < 0) {
+        throw new Error('Invalid product weight');
+      }
+      const volume = (product.largura * product.altura * product.profundidade) * 0.000001;
+      const densidade = product.peso / volume;
+      const frete = (1000 * volume * (densidade / 100)) * item.quantity;
+      freight += frete > 10 ? Math.round(frete) : 10;
     }
-    if (product.peso < 0) {
-      throw new Error("Invalid product weight");
-    }
-    const volume = (product.largura * product.altura * product.profundidade) * 0.000001;
-    const densidade = product.peso / volume;
-    const frete = 1000 * volume * (densidade / 100);
-    return frete > 10 ? Math.round(frete) : 10;
+    return freight;
   }
 }
--- test/freightCalculator.test.ts
+++ test/freightCalculator.test.ts
@@ -1,63 +1,37 @@
+import { MyDatabase } from '../src/Database';
 import { FreightCalculator } from '../src/use-cases/FreightCalculator';
 
-const freightCalculator = new FreightCalculator();
+const database = new MyDatabase();
+const freightCalculator = new FreightCalculator(database);
 
 describe('FreightCalculator', () => {
   test("Deve gerar exceção quando alguma dimensão do produto é negativa", async function () {
-		const input = {
-			id_product: 5,
-			description: 'D',
-			price: 30,
-			largura: -100,
-			altura: 30,
-			profundidade: 10,
-			peso: 3,
-			currency: 'BRL'
-		};
-		expect(() => freightCalculator.execute(input)).toThrow("Invalid product dimension");
+		const input = [
+			{ idProduct: 5, quantity: 1 }
+		];
+		await expect(() => freightCalculator.execute(input)).rejects.toThrow("Invalid product dimension");
 	});
 	
 	test("Deve gerar exceção quando o peso do produto é negativo", async function () {
-		const input = {
-			id_product: 6,
-			description: 'E',
-			price: 30,
-			largura: 100,
-			altura: 30,
-			profundidade: 10,
-			peso: -3,
-			currency: 'BRL'
-		};
-		expect(() => freightCalculator.execute(input)).toThrow("Invalid product weight");
+		const input = [
+			{ idProduct: 6, quantity: 1 }
+		];
+		await expect(() => freightCalculator.execute(input)).rejects.toThrow("Invalid product weight");
 	});
 	
 	test("Não deve ter valor de frete menor que $10", async function () {
-		const input = {
-			id_product: 1,
-			description: 'Camera',
-			price: 1000,
-			largura: 20,
-			altura: 15,
-			profundidade: 10,
-			peso: 1,
-			currency: 'BRL'
-		};
-		const freight = freightCalculator.execute(input);
+		const input = [
+			{ idProduct: 1, quantity: 1 }
+		];
+		const freight = await freightCalculator.execute(input);
 		expect(freight).toBe(10);
 	});
 	
 	test("Deve calcular valor de frete com base nos produtos", async function () {
-		const input = {
-			id_product: 2,
-			description: 'Geladeira',
-			price: 5000,
-			largura: 200,
-			altura: 100,
-			profundidade: 50,
-			peso: 40,
-			currency: 'BRL'
-		};
-		const freight = freightCalculator.execute(input);
+		const input = [
+			{ idProduct: 2, quantity: 1 }
+		];
+		const freight = await freightCalculator.execute(input);
 		expect(freight).toBe(400);
 	});
 });
