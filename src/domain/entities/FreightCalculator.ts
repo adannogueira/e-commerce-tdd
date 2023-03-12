d3143cbeb1472ac932bb6d4894990e350c05de64
Adan Nogueira <adan.nogueira@gmail.com>
2023-03-12 13:12:15 -0300

    feat: ensure FreightCalculator receives the distance

--- src/domain/entities/FreightCalculator.ts
+++ src/domain/entities/FreightCalculator.ts
@@ -1,8 +1,8 @@
 import { Product } from './Product';
 
 export class FreightCalculator {
-  static calculate(product: Product) {
-    const frete = (1000 * product.calculateVolume() * (product.calculateDensity() / 100));
+  static calculate(product: Product, distance: number) {
+    const frete = (distance * product.calculateVolume() * (product.calculateDensity() / 100));
     return frete > 10 ? Math.round(frete) : 10;
   }
 }
--- test/freightCalculator.test.ts
+++ test/freightCalculator.test.ts
@@ -1,6 +1,8 @@
 import { FreightCalculator } from '../src/domain/entities/FreightCalculator';
 import { Product } from '../src/domain/entities/Product';
 
+const DEFAULT_DISTANCE = 1000;
+
 describe('FreightCalculator', () => {
 	test("Não deve ter valor de frete menor que $10", function () {
 		const input = new Product({
@@ -13,7 +15,7 @@ describe('FreightCalculator', () => {
 			weight: 1,
 			currency: 'BRL'
 		});
-		const freight = FreightCalculator.calculate(input);
+		const freight = FreightCalculator.calculate(input, DEFAULT_DISTANCE);
 		expect(freight).toBe(10);
 	});
 	
@@ -28,7 +30,25 @@ describe('FreightCalculator', () => {
 			weight: 40,
 			currency: 'BRL'
 		});
-		const freight = FreightCalculator.calculate(input);
+		const freight = FreightCalculator.calculate(input, DEFAULT_DISTANCE);
 		expect(freight).toBe(400);
+	});
+
+	test.each([
+		[1000, 400],
+		[2000, 800]
+	])("Deve calcular valor de frete com base na distância", function (distance, expectedFreight) {
+		const items = new Product({
+			idProduct: 2,
+			description: 'Geladeira',
+			price: 5000,
+			width: 200,
+			height: 10,
+			length: 50,
+			weight: 40,
+			currency: 'BRL'
+		});
+		const freight = FreightCalculator.calculate(items, distance);
+		expect(freight).toBe(expectedFreight);
 	});
 });
