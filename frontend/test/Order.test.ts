b1c0e56e74bf58983826814f9e79e1a401773146
Adan Nogueira <adan.nogueira@gmail.com>
2023-04-21 17:21:11 -0300

    test: add order entity tests

---
+++ frontend/test/Order.test.ts
@@ -0,0 +1,15 @@
+import { Order } from '../src/domain/Order'
+import { Product } from '../src/domain/Product'
+
+it('should create an empty order', () => {
+  const order = new Order('123.456.789-10', '29560000')
+  expect(order.getTotal()).toBe('R$ 0,00')
+})
+
+it('should create an order with 3 items', () => {
+  const order = new Order('123.456.789-10', '29560000')
+  order.addItem(new Product(1, 'A', 1000))
+  order.addItem(new Product(2, 'B', 5000))
+  order.addItem(new Product(3, 'C', 30))
+  expect(order.getTotal()).toBe('R$ 6.030,00')
+})
