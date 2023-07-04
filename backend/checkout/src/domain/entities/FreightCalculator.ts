4ea298581f775469566ccf6edb95a1a69ca95ce5
Adan Nogueira <adan.nogueira@gmail.com>
2023-07-04 08:26:34 -0300

    refactor: freight calculator rounding strategy

--- backend/checkout/src/domain/entities/FreightCalculator.ts
+++ backend/checkout/src/domain/entities/FreightCalculator.ts
@@ -3,6 +3,6 @@ import { Product } from './Product';
 export class FreightCalculator {
   static calculate(product: Product, distance: number) {
     const frete = (distance * product.calculateVolume() * (product.calculateDensity() / 100));
-    return frete > 10 ? Math.round(frete) : 10;
+    return frete > 10 ? Math.round(frete * 100) / 100 : 10;
   }
 }
